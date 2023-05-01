3c5c42a65c2fb296b645a37b05a0e7b84ab5e92f
Костя Луценко <lutsenkos2016@gmail.com>
2023-05-01 12:02:50 +0300

    Update Fib.java

--- src/Fib.java
+++ src/Fib.java
@@ -3,14 +3,11 @@ import parcs.*;
 public class Fib implements AM{
     public void run(AMInfo info){
         long n,m,i0,k,r;
-        r = 1;
-        System.out.println("here");
-
-        info.parent.write(r);
         n = info.parent.readLong();
         m = info.parent.readLong();
         i0= info.parent.readLong();
         k = info.parent.readLong();
+        r = 1;
         for (long i=i0; i<n; i+=k)
         {
             r = r*(i+1)%m;
