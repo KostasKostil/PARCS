feb51b64bceca0f3add4b9f1b0b209795e28dd47
Костя Луценко <lutsenkos2016@gmail.com>
2023-05-01 11:19:58 +0300

    Update Fib.java

--- src/Fib.java
+++ src/Fib.java
@@ -2,26 +2,18 @@ import parcs.*;
 
 public class Fib implements AM{
     public void run(AMInfo info){
-        long n,r1,r2,r;
+        long n,m,i0,k,r;
+        r = 1;
+        System.out.println("here");
+
+        info.parent.write(r);
         n = info.parent.readLong();
-
-        //System.out.println("n="+n);
-        if (n<2) r=n;
-        else {
-            point p1 = info.createPoint();
-            channel c1 = p1.createChannel();
-
-            p1.execute("Fib");
-            c1.write(n-2);
-            point p2 = info.createPoint();
-            channel c2 = p2.createChannel();
-            p2.execute("Fib");
-            c2.write(n-1);
-
-            r1=c1.readLong();
-            r2=c2.readLong();
-            //if ((r1==0)||(r2==0)) System.out.println("n="+n+" r1="+r1+" r2="+r2);
-            r=r1+r2;
+        m = info.parent.readLong();
+        i0= info.parent.readLong();
+        k = info.parent.readLong();
+        for (long i=i0; i<n; i+=k)
+        {
+            r = r*(i+1)%m;
         }
         info.parent.write(r);
     }
