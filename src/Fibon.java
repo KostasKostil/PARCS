816f98c642347ff1ee22c7c7e32e0798bf03bd3b
Костя Луценко <lutsenkos2016@gmail.com>
2023-05-01 11:09:21 +0300

    revert

--- src/Fibon.java
+++ src/Fibon.java
@@ -11,33 +11,31 @@ public class Fibon implements AM {
     }
 
     public void run(AMInfo info) {
-      long n, m;
-      try {
+        long n;
+        try {
           /*byte[] buf = new byte[200];
           System.out.print("Enter n: ");
           System.in.read(buf);
           n=new Long(new String(buf).trim()).longValue();*/
 
-          BufferedReader in = new BufferedReader(new FileReader(info.curtask.findFile("Fibon.data")));
-          n = new Long(in.readLine()).longValue();
-          m = new Long(in.readLine()).longValue();
-      } catch (IOException e) {e.printStackTrace(); return;}
-      
-      point p1 = info.createPoint();
-      channel c1 = p1.createChannel();
-      p1.execute("Fib");
-      c1.write(n);
-      c1.write(m);
+            BufferedReader in = new BufferedReader(new FileReader(info.curtask.findFile("Fibon.data")));
+            n = new Long(in.readLine()).longValue();
+        } catch (IOException e) {e.printStackTrace(); return;}
 
-      System.out.println("Waiting for result...");
-      long r = c1.readLong();
-      System.out.println("Result found.");
+        point p1 = info.createPoint();
+        channel c1 = p1.createChannel();
+        p1.execute("Fib");
+        c1.write(n);
 
-      //System.out.println("F"+n+"="+r);
-      try{
-          PrintWriter out = new PrintWriter(new FileWriter(info.curtask.addPath("Fibon.res")));
-          out.println(r);
-          out.close();
-      } catch (IOException e) {e.printStackTrace(); return;}
+        System.out.println("Waiting for result...");
+        long r = c1.readLong();
+        System.out.println("Result found.");
+
+        //System.out.println("F"+n+"="+r);
+        try{
+            PrintWriter out = new PrintWriter(new FileWriter(info.curtask.addPath("Fibon.res")));
+            out.println(r);
+            out.close();
+        } catch (IOException e) {e.printStackTrace(); return;}
     }
 }
